6966f25f7ebd56efb0c9eadba37ec56823d3b714
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-09-21 21:57:05 +0530

    Designed Home page 🎨

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ui/button"
+import Filters from "@/components/Filters"
+import SearchForm from "@/components/SearchForm"
 
+// Keeping the main page for Server side render for the Performance
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Home Page</h1>
-      <Button>Click me</Button>
-    </div>
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
+            JSM Pro Resources
+          </h1>
+        </div>
+        <SearchForm />
+      </section>
+
+      <Filters />
+    </main>
   )
 }
 
