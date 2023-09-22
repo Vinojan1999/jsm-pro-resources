c20ae514790869bd21a48e737034eb13e9db5b59
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-09-23 00:30:20 +0530

    ResourcesPlaylist is done 📑📎

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import Filters from "@/components/Filters"
 import ResourceCard from "@/components/ResourceCard";
 import ResourceHeader from "@/components/ResourceHeader";
 import SearchForm from "@/components/SearchForm"
-import { getResources } from "@/sanity/actions"
+import { getResources, getResourcesPlaylist } from "@/sanity/actions"
 
 // To remove the cache and refresh the resources in every 900s (15min)
 export const revalidate = 900;
@@ -20,6 +20,9 @@ const HomePage = async ({ searchParams } : Props) => {
     page: '1'
   });
 
+  const resourcesPlaylist = await getResourcesPlaylist();
+  console.log(resourcesPlaylist);
+  
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
@@ -50,6 +53,7 @@ const HomePage = async ({ searchParams } : Props) => {
                   id={resource._id}
                   image={resource.image}
                   downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
                 />
               ))
             ) : (
@@ -60,6 +64,26 @@ const HomePage = async ({ searchParams } : Props) => {
           </div>
         </section>
       )}
+
+      {resourcesPlaylist.map((item: any) => (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <h1 className="heading3 self-start text-white-800">
+            {item.title}
+          </h1>
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {item.resources.map((resource: any) => (
+              <ResourceCard 
+                key={resource._id}
+                title={resource.title}
+                id={resource._id}
+                image={resource.image}
+                downloadNumber={resource.views}
+                downloadLink={resource.downloadLink}
+              />
+            ))}
+          </div>
+        </section>
+      ))}
     </main>
   )
 }
