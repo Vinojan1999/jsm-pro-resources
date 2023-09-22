aea2a4019773a2d0f20d437b611f4bc8546bd8b8
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-09-22 19:40:35 +0530

    Resources card added 📋

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,20 @@
 import Filters from "@/components/Filters"
+import ResourceCard from "@/components/ResourceCard";
 import SearchForm from "@/components/SearchForm"
+import { getResources } from "@/sanity/actions"
+
+// To remove the cache and refresh the resources in every 900s (15min)
+export const revalidate = 900;
 
 // Keeping the main page for Server side render for the Performance
-const HomePage = () => {
+const HomePage = async () => {
+
+  const resources = await getResources({
+    query: '',
+    category: '',
+    page: '1'
+  });
+
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
@@ -15,6 +27,28 @@ const HomePage = () => {
       </section>
 
       <Filters />
+
+      <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+        Header
+
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          {resources?.length > 0 ? (
+            resources.map((resource: any) => (
+              <ResourceCard 
+                key={resource._id}
+                title={resource.title}
+                id={resource._id}
+                image={resource.image}
+                downloadNumber={resource.views}
+              />
+            ))
+          ) : (
+            <p className="bado-regular text-white-400">
+              No resources found!
+            </p>
+          )}
+        </div>
+      </section>
     </main>
   )
 }
