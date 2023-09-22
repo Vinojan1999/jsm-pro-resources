e3233d2b706986996ff86f0a177e19fe20dcabd9
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-09-22 14:16:24 +0530

    Created schema object 📊

---
+++ sanity/schemas/resource.schema.ts
@@ -0,0 +1,52 @@
+const schema = {
+  name: 'resource',
+  title: 'Resource',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      require,
+      validation: (Rule: any) => Rule.required()
+    },
+    {
+      name: 'slug',       // slug means, the route path for each files (/resources/jsnvdj3434kjndv1k23j)
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'title' }
+    },
+    {
+      name: 'downloadLink',
+      title: 'Download Link',
+      type: 'url',
+      validation: (Rule: any) => Rule.required()
+    },
+    {
+      name: 'views',
+      title: 'Views',
+      type: 'number',
+      initialValue: 0,
+    }, 
+    {
+      name: 'poster',
+      title: 'Poster',
+      type: 'image',
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        list: ['frontend', 'backend', 'nest 13', 'fullstack', 'other']
+      }
+    }
+  ]
+}
+
+export default schema;
